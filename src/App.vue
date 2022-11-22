e2eb63e35b23a3801045b16e95ebb5e18a6b982b
LeandroCTaddeo <leandrotaddeo03@gmail.com>
2022-11-21 22:21:52 -0300

    Incorporo Vuex al proyecto “Juego de colores” convertido a Vue CLI en el TP5

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navegacion></Navegacion>
+  </div>
+</template>
+
+<script>
+import Navegacion from './components/Navegacion.vue'
+export default {
+  name: 'App',
+  components: {
+    Navegacion
+  },
+}
+</script>
+
+<style></style>
